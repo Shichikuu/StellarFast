097cbcc10f8444eaaa054b9ee4c108b7341baa6d
Shichikuu <85778711+Shichikuu@users.noreply.github.com>
2024-12-18 01:32:38 +0700

    updated sql

--- StellarFestDB.sql
+++ StellarFestDB.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Dec 17, 2024 at 03:32 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: 127.0.0.1
+-- Generation Time: Dec 17, 2024 at 07:31 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `StellarFestDB`
+-- Database: `stellarfestdb`
 --
 
 -- --------------------------------------------------------
@@ -63,6 +63,13 @@ CREATE TABLE `users` (
   `userPassword` varchar(255) NOT NULL,
   `userRole` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userId`, `userEmail`, `userName`, `userPassword`, `userRole`) VALUES
+('69d57d90-f304-4400-86f7-4efd62bc05e0', 'admin', 'admin', 'admin', 'Admin');
 
 --
 -- Indexes for dumped tables
